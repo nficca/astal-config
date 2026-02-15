60ac9f0a6cad446a88404e912cd0031907c48133
Nic Ficca <nicficca@gmail.com>
2026-02-15 14:24:20 -0500

    Getting started

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,29 @@
 import app from "ags/gtk4/app";
+import { Astal } from "ags/gtk4";
 import { Bar } from "./components/Bar";
+import { createBinding, For, This } from "gnim";
+
+const { TOP, LEFT, RIGHT } = Astal.WindowAnchor;
 
 app.start({
     main() {
-        return <Bar />;
+        const monitors = createBinding(app, "monitors");
+
+        return (
+            <For each={monitors}>
+                {monitor => (
+                    <This this={app}>
+                        <window
+                            visible
+                            gdkmonitor={monitor}
+                            anchor={TOP | LEFT | RIGHT}
+                            application={app}
+                        >
+                            <Bar monitor={monitor} />
+                        </window>
+                    </This>
+                )}
+            </For>
+        );
     },
 });
--- src/components/Bar.tsx
+++ src/components/Bar.tsx
@@ -1,39 +1,14 @@
-import { createState, createComputed } from "ags";
+import Gdk from "gi://Gdk?version=4.0";
 
-export function Bar() {
-    return (
-        <window visible>
-            <box>
-                Click the button.
-                <LogButton
-                    buttonLabel="Click me!!"
-                    logMessage="I've been clicked!"
-                />
-            </box>
-        </window>
-    );
-}
-
-type LogButtonProps = {
-    buttonLabel: string;
-    logMessage: string;
+export type BarProps = {
+    monitor: Gdk.Monitor;
 };
 
-function LogButton({ buttonLabel, logMessage }: LogButtonProps) {
-    const [count, setCount] = createState(0);
-    const countLabel = createComputed(() => `Count: ${count()}`);
-
-    function onClicked() {
-        console.log(logMessage);
-        setCount(value => value + 1);
-    }
-
+export function Bar({ monitor }: BarProps) {
     return (
         <box>
-            <label label={countLabel} />
-            <button onClicked={onClicked}>
-                <label label={buttonLabel} />
-            </button>
+            <label label={`Using monitor: ${monitor}`} />
+            <button label="Click me!" />
         </box>
     );
 }
