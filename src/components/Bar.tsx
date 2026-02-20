1493d4201848a19d06c3daaddc1e22ddd3bc0ff4
Nic Ficca <nicficca@gmail.com>
2026-02-19 20:19:29 -0500

    Add power menu

--- src/components/Bar.tsx
+++ src/components/Bar.tsx
@@ -1,6 +1,7 @@
 import { NiriWorkspaces } from "./NiriWorkspaces";
 import { DateTime } from "./DateTime";
 import { Audio } from "./Audio";
+import { Power } from "./Power";
 import { Gdk, Gtk } from "ags/gtk4";
 
 export type BarProps = {
@@ -16,6 +17,7 @@ export function Bar({ monitor }: BarProps) {
             <box $type="end" spacing={8}>
                 <Audio scroll_step={5} />
                 <DateTime />
+                <Power />
             </box>
         </centerbox>
     );
---
+++ src/components/Power.tsx
@@ -0,0 +1,38 @@
+import { Gtk } from "ags/gtk4";
+import AstalIO from "gi://AstalIO?version=0.1";
+
+export function Power() {
+    return (
+        <menubutton class="power bar-segment">
+            <image iconName="system-shutdown-symbolic" />
+            <PowerPopover />
+        </menubutton>
+    );
+}
+
+function PowerPopover() {
+    return (
+        <popover hasArrow={false} autohide={true}>
+            <box orientation={Gtk.Orientation.VERTICAL} spacing={4}>
+                <PowerPopoverItem label="Lock" exec="hyprlock" />
+                <PowerPopoverItem label="Logout" exec="niri msg action quit" />
+                <PowerPopoverItem label="Suspend" exec="systemctl suspend" />
+                <PowerPopoverItem label="Reboot" exec="systemctl reboot" />
+                <PowerPopoverItem label="Shutdown" exec="systemctl poweroff" />
+            </box>
+        </popover>
+    );
+}
+
+type PowerPopoverItemProps = {
+    label: string;
+    exec: string;
+};
+
+function PowerPopoverItem({ label, exec }: PowerPopoverItemProps) {
+    return (
+        <button onClicked={() => AstalIO.Process.exec_async(exec, null)}>
+            <label label={label} halign={Gtk.Align.START} />
+        </button>
+    );
+}
